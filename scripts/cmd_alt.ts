524ca9ec8553b4407658eae043663292d587f83c
Hironori Wada <hironori104@gmail.com>
2024-08-04 17:03:54 +0900

    ref

--- scripts/cmd_alt.ts
+++ scripts/cmd_alt.ts
@@ -5,8 +5,6 @@ import { decodeKey, dumpKeybidingsPath, encodeKey } from "./common.ts";
 
 export async function main() {
   const contents = await Deno.readTextFile(dumpKeybidingsPath);
-  console.log(contents);
-
   const config = JSONC.parse(contents) as {
     key: string;
     command: string;
@@ -17,20 +15,18 @@ export async function main() {
     const replaced = encodeKey(
       decodeKey(k.key).map((step) => step.map((k) => (k === "cmd" ? "alt" : k)))
     );
-    if (
-      replaced !== k.key &&
-      !config.some(
-        (c) =>
-          c.key === replaced && c.command === k.command && c.when === k.when
-      )
-    ) {
+    if (replaced === k.key) {
+      return [k];
+    }
+    const isAliasNotFound = !config.some(
+      (c) => c.key === replaced && c.command === k.command && c.when === k.when
+    );
+    if (isAliasNotFound) {
       return [k, { ...k, key: replaced }];
     }
     return [k];
   });
 
-  // const configPath = getVscodeUserConfigPath();
-  // const toPath = `${configPath}/keybindings.json`;
   await Deno.writeFile(
     dumpKeybidingsPath,
     new TextEncoder().encode(JSON.stringify(res, null, 4))
